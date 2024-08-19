6be91a83512cb93d0795bebdf4b29c385ca2d423
Nikola Petrovic <petrovicnik96@gmail.com>
2024-08-19 20:45:31 +0200

    Add index interface

---
+++ Repository/Interfaces/IIndex.cs
@@ -0,0 +1,14 @@
+﻿namespace Custom_Database.Repository.Interfaces;
+
+internal interface IIndex<K, V>
+{
+    /// <summary>
+    /// Create a new entry in this index that maps key K to value V
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    Task InsertAsync(K key, V value);
+
+    Task<Tuple<K, V>> GetAsync(K key);
+}
