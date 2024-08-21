aa529b99334bc041bba00f1458ea5bae171e2e6f
Nikola Petrovic <petrovicnik96@gmail.com>
2024-08-21 23:10:19 +0200

    Add IRecordStorage and RecordStorage for creating another layer on top of IBlockS torage that uses the blocks to make up variable length records.

---
+++ Core/Interfaces/IRecordStorage.cs
@@ -0,0 +1,11 @@
+﻿namespace Custom_Database.Core.Interfaces;
+
+internal interface IRecordStorage
+{
+    Task UpdateAsync(uint recordId, byte[] data);
+    Task<byte[]> FindAsync(uint recordId);
+    Task<uint> CreateAsync();
+    Task<uint> CreateAsync(byte[] data);
+    Task<uint> CreateAsync(Func<uint, byte[]> dataGenerator);
+    Task DeleleAsync(uint recordId);
+}
---
+++ Core/RecordStorage.cs
@@ -0,0 +1,36 @@
+﻿using Custom_Database.Core.Interfaces;
+
+namespace Custom_Database.Core;
+
+class RecordStorage : IRecordStorage
+{
+    public Task<uint> CreateAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<uint> CreateAsync(byte[] data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<uint> CreateAsync(Func<uint, byte[]> dataGenerator)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleleAsync(uint recordId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<byte[]> FindAsync(uint recordId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(uint recordId, byte[] data)
+    {
+        throw new NotImplementedException();
+    }
+}
